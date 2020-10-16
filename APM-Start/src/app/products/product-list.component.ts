0224c3e5bb5b019fcbd00ae672e46292e5c52f16
Pablo <pawel.stolka@gmail.com>
2020-10-16 13:16:52 +0200

    async pipe

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -10,15 +11,16 @@ import { ProductService } from './product.service';
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
-  products: Product[] = [];
+  products$: Observable<Product[]>;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(
-      products => this.products = products,
-      error => this.errorMessage = <any>error
-    );
+    this.products$ = this.productService.getProducts()
+      // .subscribe(
+      //   products => this.products = products,
+      //   error => this.errorMessage = <any>error
+      // );
   }
 
   onAdd(): void {
