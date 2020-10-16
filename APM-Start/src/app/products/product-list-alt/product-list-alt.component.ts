5d0be81712801d8ff7a43840c2175c5d43dd677c
Pablo <pawel.stolka@gmail.com>
2020-10-16 14:22:41 +0200

    alt async

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -10,14 +10,14 @@ import { ProductService } from '../product.service';
 export class ProductListAltComponent  {
   pageTitle = 'Products';
   errorMessage = '';
-  products: Product[] = [];
+  products = this.productService.products$
 
   constructor(
     private productService: ProductService
   ) {}
 
   ngOnInit(): void {
-    this.productService.getProducts()
+    // this.productService.getProducts()
     // this.productService.getProducts().subscribe(
     //   products => this.products = products,
     //   error => this.errorMessage = <any>error
